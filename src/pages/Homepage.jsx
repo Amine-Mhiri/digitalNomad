bda9215a047d2a41aad325f6ec708a84ad883b2b
unknown <mhiriamin@gmail.com>
2023-12-07 15:54:09 +0100

    First Commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Digital Nomad</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
+}
+
+export default Homepage;
