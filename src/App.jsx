15e27d94af3f556839fa762445ac8ba9c7f9b9b6
unknown <mhiriamin@gmail.com>
2023-12-09 17:17:28 +0100

    Building App Layout + Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 
 import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/appLayout";
+import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of Cities</p>} />
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
